90c3448f1dac109bfb1108dd06662e7e6ab6ace5
Andrew Masiero <amasiero@adams-debian.lan>
2024-01-13 23:12:48 -0600

    added timer and score foundation

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ import {
 // ELEMENTS
 const currentProblemSection = collectElements(
   document.querySelector("#current-problem > div").children,
-  ["currentButton", "currentInput", "currentImage"]
+  ["currentButton", "currentInput", "currentImage"],
 );
 const setupForm = document.getElementById("setup-form");
 let arithmaticList = checkSetup(setupForm);
@@ -22,7 +22,7 @@ updateCurrentProblem(currentProblemSection);
 
 const lastProblemSection = collectElements(
   document.querySelector("#last-problem > div").children,
-  ["lastButton", "lastInput", "lastImage"]
+  ["lastButton", "lastInput", "lastImage"],
 );
 lastProblemSection.lastTitle = document.querySelector("#last-problem > h2");
 
@@ -38,8 +38,10 @@ currentProblemSection.currentInput.addEventListener("keypress", (e) => {
     currentProblemSection.currentProblem = generateEquation(
       1,
       9,
-      arithmaticList
+      arithmaticList,
     );
     updateCurrentProblem(currentProblemSection);
   }
 });
+
+// TIMER
